fa6c89c869de3f276306805c2e109f3be74ea68f
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-06-25 17:21:11 +0330

    feat: add Supabase authentication

---
+++ src/lib/supabase.js
@@ -0,0 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl =
+  "https://ntpncphdioldiskcdylf.supabase.co";
+
+const supabaseKey =
+  "sb_publishable_FUdULaGw-9RBHN_RVKDPpg_yskxLtfK";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
