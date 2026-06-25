15398658395db514c5bc8e5baea1208ef00ba2e3
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-06-25 22:07:22 +0330

    fixed

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,13 +1,9 @@
-import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  "https://ntpncphdioldiskcdylf.supabase.co";
+ import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://ntpncphdioldiskcdylf.supabase.co";
 
 const supabaseKey =
   "sb_publishable_FUdULaGw-9RBHN_RVKDPpg_yskxLtfK";
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-);
- 
+export const supabase = createClient(supabaseUrl, supabaseKey);
