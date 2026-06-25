87096688904044246ca62de5814f989815ce5dfb
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-06-25 17:18:13 +0330

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,37 +1,38 @@
 import { useNavigate } from "react-router-dom";
 import Searchbox from "./Searchbox";
 import Profile from "./Profile";
-import Notification from "./Notification";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="flex items-center justify-between">
-      {/* Left */}
-      <div className="text-xl font-bold">EduFlow</div>
+    <>
+      <nav className="flex items-center justify-between p-4 border-b">
+        {/* Left */}
+        <div className="font-bold">EduFlow</div>
 
-      {/* Right */}
-      <div className="flex items-center gap-4">
-        <button
-          onClick={() => navigate("/HomePage")}
-          className="px-3 py-1 border rounded hover:bg-gray-100"
-        >
-          Home Page
-        </button>
+        {/* Right */}
+        <div className="flex items-center gap-4">
+          <button
+            onClick={() => navigate("/home")}
+            className="px-3 py-1 border rounded hover:bg-gray-100"
+          >
+            Home Page
+          </button>
 
-        <button
-          onClick={() => navigate("/dashboard")}
-          className="px-3 py-1 border rounded hover:bg-gray-100"
-        >
-          Dashboard
-        </button>
+          <button
+            onClick={() => navigate("/dashboard")}
+            className="px-3 py-1 border rounded hover:bg-gray-100"
+          >
+            Dashboard
+          </button>
 
-        <Searchbox />
-        <Notification />
-        <Profile />
-      </div>
-    </nav>
+          <Searchbox />
+
+          <Profile />
+        </div>
+      </nav>
+    </>
   );
 };
 
--- src/components/Navbar/Searchbox.jsx
+++ src/components/Navbar/Searchbox.jsx
@@ -1,18 +1,19 @@
 import { IoSearch } from "react-icons/io5";
 import { useState } from "react";
+
 const Searchbox = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [search, setSearch] = useState("");
 
   return (
-    <div className="flex items-center border rounded-lg px-4 py-2 w-full max-w-md bg-white">
-      <IoSearch className="text-gray-500 mr-2" size={20} />
+    <div className="flex items-center gap-2 border rounded px-2 py-1">
+      <IoSearch className="text-gray-500" />
 
       <input
         type="text"
         placeholder="Search..."
         className="w-full outline-none text-sm"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
     </div>
   );
