ae3efeb26143821b9b4e897f43eb620bc684ef72
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-06-25 22:11:47 +0330

    to help login.jsx perfomance

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+
+
+export const useAuth = () => {
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const init = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+
+      setUser(user);
+
+      if (user) {
+        const { data } = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", user.id)
+          .single();
+
+        setProfile(data);
+      }
+
+      setLoading(false);
+    };
+
+    init();
+
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_, session) => {
+        setUser(session?.user ?? null);
+      }
+    );
+
+    return () => listener.subscription.unsubscribe();
+  }, []);
+
+  return { user, profile, loading };
+};
