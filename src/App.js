aadeaddca0926c7904502626d612a950ea53eb2a
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-06-25 17:15:54 +0330

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import "./index.css";
-import Navbar from "./components/Navbar/Navbar";
-import HomePage from "./components/HomePage";
-import Dashboard from "./components/DashBoard/Dashboard";
-
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { supabase } from "./lib/supabase";
+
+
+import "./index.css";
+
+import Login from "./components/Login";
+import HomePage from "./components/HomePage";
+import Dashboard from "./components/DashBoard/Dashboard.jsx"
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data }) => {
+      setUser(data.user);
+      setLoading(false);
+    });
+
+    const { data: listener } =
+      supabase.auth.onAuthStateChange((_, session) => {
+        setUser(session?.user ?? null);
+      });
+
+    return () => listener.subscription.unsubscribe();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <BrowserRouter>
-      <Navbar />
-
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={user ? <HomePage /> : <Login />}
+        />
+        <Route
+          path="/dashboard"
+          element={user ? <Dashboard /> : <Login />}
+        />
       </Routes>
     </BrowserRouter>
   );
