8cce35d1f7fbd075a756246af295f920d59db501
raha mohebbi <raha.mohebbi.03@gmail.com>
2026-07-03 12:05:10 +0330

    adding CourseCards component to this

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,13 @@
 import TopInstructors from "./TopInstructors";
 import Login from "./Login";
 import Navbar from "../components/Navbar/Navbar";
+import CourseCards from "./DashBoard/CourseCards";
 const HomePage = () => {
   return (
     <div className="home-page">
     <Navbar />
       <TopInstructors />
+      <CourseCards/>
     </div>
   );
 };
